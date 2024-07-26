77ff260243ed687f153a928a44c2ca4c167845b6
wogha95 <wogha95@naver.com>
2024-07-26 22:39:26 +0900

    4-7) 병목 함수에 memoization 적용

---
+++ src/utils/memoize.js
@@ -0,0 +1,18 @@
+export function memoize(fn) {
+  const cache = {};
+
+  return function (...args) {
+    if (args.length !== 1) {
+      return fn(args);
+    }
+
+    if (cache.hasOwnProperty(args)) {
+      return cache[args];
+    }
+
+    const result = fn(args);
+    cache[args] = result;
+
+    return result;
+  }
+}
